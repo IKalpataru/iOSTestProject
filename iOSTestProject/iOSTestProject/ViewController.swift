a4c692dbd632313d053f423605b10204401c6cec
Kalpataru Sahoo <kalpataru.s@telliant.net>
2022-07-21 13:25:45 +0530

    initial Commit

---
+++ iOSTestProject/iOSTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSTestProject
+//
+//  Created by Kalpataru Sahoo on 21/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
